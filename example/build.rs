f8c8247d0f508543be42f5951ee323359b22dc93
Aistis Raulinaitis <sheganinans@gmail.com>
2017-08-19 16:59:35 -0700

    New cheddar version

--- example/build.rs
+++ example/build.rs
@@ -1,5 +1,8 @@
 extern crate cheddar;
 
 fn main() {
-    cheddar::Cheddar::new().expect("could not read manifest")
-        .insert_code("#include <urweb.h>").run_build("ffi.h"); }
+    cheddar::Builder::c99().expect("could not read cargo manifest")
+        .name("ffi.h")
+        .insert_code("#include <urweb.h>")
+        .output_directory(".")
+        .run_build(); }
